10463b483b4bed01f180968c5413aeb7a21a5b87
Brett Anda <brett@brettanda.ca>
2022-10-13 12:57:00 -0600

    actually added a redirects file

---
+++ middleware/redirects.global.ts
@@ -0,0 +1,6 @@
+export default defineNuxtRouteMiddleware((to, from) => {
+  // const url = to.path;
+  // if (url.slice(-1, url.length) == "/" && to.path != "/") {
+  //   return navigateTo(url.slice(0, -1), { replace: true })
+  // }
+})

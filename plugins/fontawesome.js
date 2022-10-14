87b08dd854653a6587bc45f240107f8165e85fbf
Brett Anda <brett@brettanda.ca>
2022-10-14 13:51:04 -0600

    catchup - added dev.to link to footer

--- plugins/fontawesome.js
+++ plugins/fontawesome.js
@@ -1,7 +1,7 @@
 import { library, config } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 // import { faBars, faLocationDot } from '@fortawesome/free-solid-svg-icons'
-import { faGithub, faCodepen, faLinkedin, faFacebook } from '@fortawesome/free-brands-svg-icons'
+import { faGithub, faCodepen, faLinkedin, faFacebook, faDev } from '@fortawesome/free-brands-svg-icons'
 import { faSun, faMoon, faLink, faDesktop } from '@fortawesome/free-solid-svg-icons'
 
 // This is important, we are going to let Nuxt.js worry about the CSS
@@ -9,7 +9,7 @@ config.autoAddCss = false
 
 // You can add your icons directly in this plugin. See other examples for how you
 // can add other styles or just individual icons.
-library.add(faGithub, faCodepen, faLinkedin, faFacebook, faSun, faMoon, faLink, faDesktop)
+library.add(faGithub, faCodepen, faLinkedin, faFacebook, faSun, faMoon, faLink, faDesktop, faDev)
 
 // Register the component globally
 export default defineNuxtPlugin(nuxtApp => {

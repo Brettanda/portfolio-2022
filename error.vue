acff4b03d02ac9e74700c7dfb7d2a85730f05cd7
Brett Anda <brett@brettanda.ca>
2025-04-29 19:29:15 -0600

    fixed the look of the error page

--- error.vue
+++ error.vue
@@ -8,18 +8,18 @@
     </Head>
     <div class="subcontainer">
       <header class="page_header container">
-        <div class="subcontainer">
+        <div class="subcontainer content-width-container">
           <h1>Error {{ error.statusCode }} - {{ error.statusMessage }}</h1>
         </div>
       </header>
-      <div class="prose">
+      <div class="prose content-width-container">
         <p>{{ error.message }}</p>
+        <p>
+          <a @click="handleError" style="--link-colour: var(--colour-3)"
+            >Go home</a
+          >
+        </p>
       </div>
-      <p>
-        <a @click="handleError" style="--link-colour: var(--colour-3)"
-          >Go home</a
-        >
-      </p>
     </div>
   </main>
   <Footer />

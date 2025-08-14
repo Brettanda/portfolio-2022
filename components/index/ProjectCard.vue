ddb421f3e86883c9c848c46e857bffc5b1abde71
Brettanda <brett@brett.brett>
2025-08-14 00:07:01 -0600

    how about now

--- components/index/ProjectCard.vue
+++ components/index/ProjectCard.vue
@@ -17,7 +17,7 @@
       v-bind:src="item.image"
       v-bind:alt="item.title"
       format="avif,webp"
-      quality="70"
+      quality="50"
       loading="lazy"
       sizes="200px"
       width="200px"
